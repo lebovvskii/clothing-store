c84742d2395846e243629e333d19827f17441d33
Nikita Lebovvskii <lebovvskii.design@ya.ru>
2023-11-16 18:44:54 +1000

    added an Outlet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Home from "./routes/home/home.component";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the Navigation </h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };

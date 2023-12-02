2497165e7df1d45dfd0f72042f152857b456ddfe
Nikita Lebovvskii <lebovvskii.design@ya.ru>
2023-12-02 13:57:09 +1000

    refactored

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -4,7 +4,7 @@ export const CartItem = ({ cartItem }) => {
   const { name, quantity, imageUrl, price } = cartItem;
   return (
     <div className="cart-item-container ">
-      <img src={imageUrl} alt={`${name}`} />
+      <img src={imageUrl} alt={name} />
       <div className="item-details">
         <span className="name">{name}</span>
         <span className="price">
